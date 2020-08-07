e96c195b5598023e25a987704b89dce691f6f634
Harshvardhan <harshvardhanvipat@gmail.com>
2020-08-07 17:11:58 +1000

    practice  of jsx and Html differences

--- src/index.js
+++ src/index.js
@@ -6,7 +6,18 @@ import ReactDOM from "react-dom";
 
 // function () can also be written as () =>
 const App = () => {
-  return <div> Hi there </div>;
+  const getButtonText = { text: "Click me !!" };
+  return (
+    <div>
+      <h1 className="header" style={{ backgroundColor: "red", color: "white" }}>
+        This is a test text
+      </h1>
+      <button style={{ border: "red solid 1px" }}>
+        {" "}
+        {getButtonText.text.toUpperCase()}
+      </button>
+    </div>
+  );
 };
 
 // take the react component and show it on the screen
